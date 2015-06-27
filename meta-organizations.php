40ccb7295a7bf459081be6164632f309189a85d0
yumashev <a.yumashev@gmail.com>
2015-06-28 02:09:51 +0500

    add meta

--- meta-organizations.php
+++ meta-organizations.php
@@ -1,6 +1,14 @@
 <?php
-
-
+/*
+Plugin Name: CasePress Structure
+Plugin URI: http://casepress.org
+Description: Плагин для описания структуры организации с различных точек зрения
+Author: CasePress
+Author URI: http://casepress.org
+GitHub Plugin URI: https://github.com/systemo-biz/casepress-structure
+GitHub Branch: master
+Version: 20150626-4
+*/
 
 /*
 Создаем организации и связанные таксономии
